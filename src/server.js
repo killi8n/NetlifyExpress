1b49d5fa551bf324ac2c6c607b7629b558719704
DonghoChoi <evals4dead@gmail.com>
2019-04-04 12:47:25 +0900

    deploy

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.status(200).json({ status: 200 });
+  return res.status(200).json({ status: 123123123 });
 });
 
 app.use(bodyParser.json());
